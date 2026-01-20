31bff7cad7a766fe03772aeca5c644058e281933
amd5570 <amd5570@rit.edu>
2026-01-19 22:56:47 -0500

    more stuff like the ticker

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 // gets the current year for the footer
 const yearSpan = document.getElementById('year');
 yearSpan.innerHTML = new Date().getFullYear();
+
+// hamburger menu functionality
+const hamburger = document.getElementById('hamburger-menu');
+const navMenu = document.getElementById('nav-menu');
+const navLinks = navMenu.querySelectorAll('a');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+// close menu when a link is clicked
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    });
+});
